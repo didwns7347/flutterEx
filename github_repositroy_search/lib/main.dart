31b50521ab27ed069363c9aacb2f8876d3bcba87
joonsoo.yang <didwns7347@gmail.com>
2024-06-04 14:45:44 +0900

    텍스트인풋 폼 실습

--- github_repositroy_search/lib/main.dart
+++ github_repositroy_search/lib/main.dart
@@ -9,12 +9,47 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const appTitle = 'Form Styling Demo';
+    return MaterialApp(
+      title: appTitle,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(appTitle),
         ),
+        body: const MyCustomForm(),
       ),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "Enter your user name"
+            ),
+          ),
+        )
+      ],
     );
   }
 }
