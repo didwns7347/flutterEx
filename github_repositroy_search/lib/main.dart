5d36ef6a6c47db4004317f376f2aa6d59ab233ad
joonsoo.yang <didwns7347@gmail.com>
2024-06-04 15:14:29 +0900

    텍스트인풋 알림으로 보여쥐

--- github_repositroy_search/lib/main.dart
+++ github_repositroy_search/lib/main.dart
@@ -22,34 +22,76 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class MyCustomForm extends StatelessWidget {
+class MyCustomForm extends StatefulWidget {
   const MyCustomForm({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a search term',
-            ),
-          ),
-        ),
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
 
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: "Enter your user name"
-            ),
-          ),
-        )
-      ],
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Retrieve Text Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: myController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(myController.text),
+              );
+            },
+          );
+        },
+        tooltip: 'Show me the value!',
+        child: Icon(Icons.text_fields),
+      ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Column(
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: <Widget>[
+  //       const Padding(
+  //         padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+  //         child: TextField(
+  //           decoration: InputDecoration(
+  //             border: OutlineInputBorder(),
+  //             hintText: 'Enter a search term',
+  //           ),
+  //         ),
+  //       ),
+
+  //       Padding(
+  //         padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 16),
+  //         child: TextFormField(
+  //           decoration: const InputDecoration(
+  //             border: UnderlineInputBorder(),
+  //             labelText: "Enter your user name"
+  //           ),
+  //         ),
+  //       )
+  //     ],
+  //   );
+  // }
 }
